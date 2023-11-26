33db71ad05b0ab104aea7b0955167b19f1d31d4c
Vats-shivam <jhaknoweverything@gmail.com>
2023-11-26 23:49:51 +0530

    solved tasks filtering bugs

--- frontend/src/components/Task/Task.jsx
+++ frontend/src/components/Task/Task.jsx
@@ -3,20 +3,30 @@
 //   return <h1>Task</h1>;
 
 import React, { useEffect, useState } from "react";
-// import { FormContext } from "../../context/TaskContext";
+import { FormContext } from "../../context/TaskContext";
 //Task=[{isDone},{}]
 //task.map(task,index)=>index===id?
 const Task = (props) => {
+  const { tasks, setTasks } = React.useContext(FormContext);
+
   // const {tasks,setTasks}=React.useContext(FormContext);
   // console.log(setTaskObj);
   // console.log(props.task);
   // props.setTaskObj(props.task);
+  
+  // for(let i=0;i<tasks.length;i++)
+  // {
+  //   if(tasks[i].title===props.task.title)
+  //   {
+  //     tasks[i].isDone=props.task.isDone;
+  //     break;
+  //   }
+  // }
 
   return (
     <div className="bg-black flex w-4/5 mx-auto justify-between py-4 px-2 rounded-2xl my-4 text-white">
       <h3>{props.task.title}</h3>
-      <input id="isComplete" type="checkbox" onChange={(e)=>{props.setTask({isDone:e.target.checked}) 
-      console.log("DOne")}}/>
+      <input id="isComplete" type="checkbox" checked={props.task.isDone} onChange={(e)=>props.handleChange(e,props.k)}/>
     </div>
   )
 }
--- frontend/src/components/TasksTab/TasksTab.jsx
+++ frontend/src/components/TasksTab/TasksTab.jsx
@@ -1,22 +1,32 @@
 import { Task } from "../";
-import React, { useEffect, useMemo } from "react";
+import React, { useCallback, useEffect, useMemo,useState} from "react";
 import { FormContext } from "../../context/TaskContext";
 // import {TaskContext} from "../../context/TaskContext";
 // keeps the collection of tasks and renders the tasks tab will be used as to do and done
 export default function TasksTab(props) {
   const { tasks, setTasks } = React.useContext(FormContext);
+  
+
   // const temp=[];
-  const [taskObj, setTaskObj] = React.useState({ title: "", isDone: false });
+  const handleStatus=useCallback((e,key)=>{
+    const temp=[...tasks];
+    temp[key].isDone=e.target.checked;
+    setTasks(temp);
+    console.log(temp[key])
+  },[tasks])
   return (
     <div className="bg-white rounded-3xl w-1/2 text-black overflow-scroll TasksTab">
       <h2 className="text-center ">{props.Title}</h2>
       {
         tasks.map((task,index) => {
           if (task.isDone === true && props.Title === "Done")
-            return <Task key={index} task={task} setTask={setTaskObj} />
+          {
+            return <Task k={index} task={task} handleChange={handleStatus}  />
+          }
           else if (task.isDone === false && props.Title === "To-Do")
-            return <Task key={index} task={task} setTask={setTaskObj} />
-          console.log(taskObj);
+          {
+            return <Task k={index} task={task} handleChange={handleStatus} />
+          }// console.log(taskObj);
 
       })
     }
